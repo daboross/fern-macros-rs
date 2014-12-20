59d3d19101b3bf9c2e80c0352b586765bacfe159
Dabo Ross <daboross@daboross.net>
2014-12-19 22:48:10 -0800

    Add some documentation and licensing information

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 #![feature(macro_rules)]
 
+//! Logging macros for the fern library. This package stores a thread-local logger, and allows access to it via log!() and <level>!() macros.
+//!
+//! Note that in order for logging_macros to function correctly, you can't rename the fern crate (using 'as'), and `fern_macros` needs to be used as plugin and link. Shown here:
+//!
+//! ```
+//! extern crate fern;
+//! #[phase(plugin, link)]
+//! extern crate fern_macros;
+//! ```
+
 extern crate fern;
 
 use std::cell;
@@ -11,12 +21,14 @@ use fern::OutputConfig;
 
 thread_local!(static DEFAULT_LOGGER: cell::RefCell<sync::Arc<Box<Logger + Sync + Send>>> = cell::RefCell::new(sync::Arc::new(OutputConfig::Stdout.into_logger().unwrap())));
 
-pub fn init_thread_logger(logger: sync::Arc<Box<Logger + Sync + Send>>) {
+#[experimental]
+pub fn init_logger(logger: sync::Arc<Box<Logger + Sync + Send>>) {
     DEFAULT_LOGGER.with(move |log| {
         *log.borrow_mut() = logger;
     });
 }
 
+#[experimental]
 pub fn log(level: &Level, msg: &str) {
     DEFAULT_LOGGER.with(|logger| {
         match logger.borrow().log(level, msg) {
@@ -30,42 +42,56 @@ pub fn log(level: &Level, msg: &str) {
     });
 }
 
+/// Logs a message with the thread logger.
 #[macro_export]
+#[experimental]
 macro_rules! log(
     ($level:expr, $($arg:tt)*) => (
         ::fern_macros::log($level, format!($($arg)*).as_slice())
     )
 );
 
+/// Logs a debug message with the thread logger.
 #[macro_export]
+#[experimental]
 macro_rules! debug(
     ($($arg:tt)*) => (
         log!(&::fern::Level::Debug, $($arg)*)
     )
 );
 
+/// Logs an informational message with the thread logger.
 #[macro_export]
+#[experimental]
 macro_rules! info(
     ($($arg:tt)*) => (
         log!(&::fern::Level::Info, $($arg)*)
     )
 );
 
+/// Logs a warning message with the thread logger.
 #[macro_export]
+#[experimental]
 macro_rules! warning(
     ($($arg:tt)*) => (
         log!(&::fern::Level::Warning, $($arg)*)
     )
 );
 
+/// Logs a severe message with the thread logger.
 #[macro_export]
+#[experimental]
 macro_rules! severe(
     ($($arg:tt)*) => (
         log!(&::fern::Level::Severe, $($arg)*)
     )
 );
 
+/// Logs an error message with the thread logger, if an error occurs.
+///
+/// This macro requires a Result<_, Show>, and will log as severe error message with the given argument format if the result happens to end up being Err(e)
 #[macro_export]
+#[experimental]
 macro_rules! log_error(
     ($result:expr, $($arg:tt)*) => (
         match $result {
@@ -75,7 +101,9 @@ macro_rules! log_error(
     )
 );
 
+/// Exactly the same as log_error!(), except also has an 'after' statement which will be run after logging the severe error message.
 #[macro_export]
+#[experimental]
 macro_rules! log_error_then(
     ($result:expr, $after:expr, $($arg:tt)*) => (
         match $result {
